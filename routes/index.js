56019d6f262a1b344c8f6c62596568e44a2ff23c
Jahnavi K.S <ksjahnavi07@gmail.com>
2021-03-13 13:04:24 +0530

    Updated files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const cool = require('cool-ascii-faces') ;
+const express = require('express');
+const router = express.Router();
+const path = require('path') ;
+const PORT = process.env.PORT || 3000
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://jahanvi123:mongo123@cluster0.igdjg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 module.exports = router;
+
+
+router.get('/db', function (request, response) {
+
+  MongoClient.connect(url, function(err, client) {
+    if(err) throw err;
+    //get collection of routes
+    const db = client.db('DB');
+    const Routes = db.collection('DB_Collection');
+    //get all Routes
+    Routes.find({ }).toArray(function (err, docs) {
+      if(err) throw err;
+
+      response.render('pages/db', {results: docs});
+
+    });
+
+    //close connection when your app is terminating.
+    client.close(function (err) {
+      if(err) throw err;
+    });
+  });//end of connect
+
+});//end router.get
+
+
+
+
+
+
+
